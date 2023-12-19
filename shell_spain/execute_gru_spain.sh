2b6b13ece3b76a0ccf1e2781fab1c69880f3e01f
Andrew <andrewlee1807@gmail.com>
2023-12-19 21:29:13 +0900

    experiment spain data

--- shell_spain/execute_gru_spain.sh
+++ shell_spain/execute_gru_spain.sh
@@ -6,12 +6,12 @@
 for i in 1 2 3 4 5 6 7 8 9 10 15 20 24 36 48 60 72 84 96 108 120 132; do
    echo "Starting to train model with output length = $i"
    python main.py \
-    --dataset_name="gru" \
+    --dataset_name="spain" \
     --write_log_file=True \
     --model_name="gru" \
     --config_path="config/gru/spain.yaml" \
     --output_length=$i \
-    --device=0 \
+    --device=1 \
     --output_dir="benchmark/exp/spain/gru"
    echo "Finished training model with output length = $i"
    echo "=================================================="
--- shell_spain/execute_mlp_spain.sh
+++ shell_spain/execute_mlp_spain.sh
@@ -6,7 +6,7 @@
 for i in 1 2 3 4 5 6 7 8 9 10 15 20 24 36 48 60 72 84 96 108 120 132; do
    echo "Starting to train model with output length = $i"
    python main.py \
-    --dataset_name="mlp" \
+    --dataset_name="spain" \
     --write_log_file=True \
     --model_name="mlp" \
     --config_path="config/mlp/spain.yaml" \
